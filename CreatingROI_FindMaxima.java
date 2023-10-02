6d0ba40790e45cc3c8ca183c304dbae54e246c9e
stephanusst <stephanusst@gmail.com>
2023-10-03 00:51:19 +0700

    Axioo
    3/10/2023

--- CreatingROI_FindMaxima.java
+++ CreatingROI_FindMaxima.java
@@ -3,47 +3,57 @@ import ij.process.*;
 import ij.gui.*;
 import java.awt.*;
 import ij.plugin.*;
+import ij.plugin.filter.*;
 
 public class CreatingROI_FindMaxima implements PlugIn {
 
 	static String title="Maxima";
-	int prominence=10;
+	int prominence=30;
 	GenericDialog gd;
 
 	public void run(String arg) {
 		//B. Image
 		//B.1. Need Opened Image
 		ImagePlus imp;
-  	        //IJ.run(imp, "IJ webcam plugin", "camera=[S0-40 0] width=1280 height=960 unit=�m pixel_size=2.00000000 interval=1000 frames=3 process");
+		//IJ.run(imp, "IJ webcam plugin", "camera=[S0-40 0] width=1280 height=960 unit=�m pixel_size=2.00000000 interval=1000 frames=3 process");
 		imp = IJ.getImage();
 
 		//B.1. Automatically Opened
-        		//ImagePlus imp = IJ.openImage("I:/ImageJ/plugins/Workspace/_images/SnapDada.tif");
+		//ImagePlus imp = IJ.openImage("I:/ImageJ/plugins/Workspace/_images/SnapDada.tif");
 		//imp.show();
 		IJ.run(imp, "16-bit", "");
+
 		//B.2. Initial
 		IJ.run(imp, "Find Maxima...", "prominence="+prominence+" exclude output=[Point Selection]");
-
-		
 		ImageProcessor ip=imp.getProcessor();
-                                //ip.snapshot(); //Makes a copy of this image's pixel data that can be later restored using reset() or reset(mask).
+		ip.snapshot(); //Makes a copy of this image's pixel data that can be later restored using reset() or reset(mask).
 
 		DialogListener listener = new DialogListener(){
 		 public boolean dialogItemChanged(GenericDialog gd, AWTEvent event){
+			  //IJ.setAutoThreshold(imp, "Mean Dark");
+   			  //IJ.run(imp, "Create Selection", "");
+			  //RoiManager rm = RoiManager.getRoiManager();
+			  //ThresholdToSelection ts= new ThresholdToSelection(ip);
+
+			  //rm.addRoi(roi);
+			  //rm.select(0);
+			  //roiManager("Select", 0);
+
 	  		  prominence=(int)gd.getNextNumber();
 	  		  IJ.run(imp, "Select None", "");
 		          IJ.run(imp, "Find Maxima...", "prominence="+prominence+" exclude output=[Point Selection]");
 			  imp.updateAndDraw();
+			  IJ.log("listener");
             		  return true;
       	 	  }
 		};
 
-      		gd = new GenericDialog("Prominence Adjuster");
-
+      		gd = new NonBlockingGenericDialog("Prominence Adjuster");
       		//gd.addImage(imp);
-		gd.addSlider("Prominence", 10, 99, 30);
+		gd.addSlider("Prominence", 10, 99, 30, 2);
      		gd.addDialogListener(listener);
       		gd.showDialog();
+		IJ.log("Run End");
 		
 		/*
 		//C. Threshold
@@ -75,6 +85,7 @@ public class CreatingROI_FindMaxima implements PlugIn {
 		//ip.fill(); 	
 
 		imp.updateAndDraw();
+
 		while (!gd.wasCanceled()) {
 	  	  gd.showDialog();
 		  prominence=(int)gd.getNextNumber();
